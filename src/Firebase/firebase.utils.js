538e37c60f31d28ecf6013aa6e24608c41a87816
Unkie <unknown920@gmail.com>
2024-05-04 15:00:34 +0300

    refactor: removed unused data

--- src/Firebase/firebase.utils.js
+++ src/Firebase/firebase.utils.js
@@ -4,20 +4,20 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: 'AIzaSyB4TFGIv41g7u6-OFl0gfJS-PUElHxyfW0',
-  authDomain: 'tavrida-selection-tool.firebaseapp.com',
-  projectId: 'tavrida-selection-tool',
-  storageBucket: 'tavrida-selection-tool.appspot.com',
-  messagingSenderId: '159482514049',
-  appId: '1:159482514049:web:bb56ab1f76f942369c9b80',
-  measurementId: 'G-21HTSSZ86C',
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: 'tavrida-selection-tool.appspot.com',
+	messagingSenderId: '159482514049',
+	appId: '1:159482514049:web:bb56ab1f76f942369c9b80',
+	measurementId: 'G-21HTSSZ86C',
 };
 
 firebase.initializeApp(config);
 export const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({
-  login_hint: 'user@example.com',
+	login_hint: 'user@example.com',
 });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
